26cae795602103da55967ac0578a74043e5bfaa0
Bikeredu <luiseduardo.tellez@gmail.com>
2020-02-09 14:29:15 -0500

    ajustes urls

--- functions.php
+++ functions.php
@@ -409,7 +409,6 @@ function get_post_primary_category($post_id, $term='category', $return_all_categ
 	return $return;
 }  
 
-
 function my_load_scripts() {
     wp_enqueue_script( 'my_js', get_theme_file_uri( 'assets/js/ajax-posts.js' ), array( 'jquery' ) );
  
